3b9e6c6ec2d984c47365d5d96615cbf880dfae3a
Clinton Forster <clintonforster@gmail.com>
2021-07-28 17:51:40 +1000

    add icons to tab navigation

--- navigation/MainNavigation.tsx
+++ navigation/MainNavigation.tsx
@@ -1,22 +1,40 @@
 import React from 'react';
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { StackNavigationOptions } from "@react-navigation/stack";
-import colors from "../constants/colors";
+import { BottomTabBarOptions, createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
 import { MainNavigationParamList } from "../types/navigation.types";
 import AboutNavigator from "./stack-navigators/AboutNavigator";
 import TourCommentaryNavigator from "./stack-navigators/TourCommentaryNavigator";
+import colors from '../constants/colors';
 
 const MainTab = createBottomTabNavigator<MainNavigationParamList>();
 
+const defaultTabBarOptions: BottomTabBarOptions = {
+  activeTintColor: colors.green,
+  inactiveTintColor: 'white',
+  style: {
+    backgroundColor: colors.purple
+  }
+}
+
 const MainNavigator = () => (
-  <MainTab.Navigator >
+  <MainTab.Navigator tabBarOptions={defaultTabBarOptions} >
     <MainTab.Screen
       name="TourCommentary"
       component={TourCommentaryNavigator}
+      options={{
+        tabBarIcon: ({ color }) => (
+          <FontAwesome name="commenting-o" size={25} color={color} />
+        )
+      }}
     />
     <MainTab.Screen
       name="About"
       component={AboutNavigator}
+      options={{
+        tabBarIcon: ({ color }) => (
+          <Ionicons name="information-circle-outline" size={25} color={color} />
+        )
+      }}
     />
   </MainTab.Navigator>
 )
