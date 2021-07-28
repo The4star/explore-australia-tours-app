ef693ef41635da56732fb013911ffb3582803693
Clinton Forster <clintonforster@gmail.com>
2021-07-28 12:17:37 +1000

    initial commit with splash screens, icons and tests added

---
+++ test/App.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import App from '../App';
+
+describe('<App />', () => {
+  it('has 1 child', () => {
+    const tree: any = renderer.create(<App />).toJSON() as renderer.ReactTestRendererJSON;
+    console.log(tree);
+
+    expect(tree.children.length).toBe(1);
+  });
+});
